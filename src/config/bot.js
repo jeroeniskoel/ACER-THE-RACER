224877fba67ebb7ce727872330428bbdd50735eb
jeroeniskoel <102921544+jeroeniskoel@users.noreply.github.com>
2026-07-03 09:45:41 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -283,7 +283,7 @@ export const botConfig = {
     defaultRole: null,
 
     // Channel ID where birthday announcements are posted.
-    announcementChannel: #birthday-announcements,
+    announcementChannel: null,
 
     // Timezone used to calculate birthday dates.
     timezone: "UTC",
